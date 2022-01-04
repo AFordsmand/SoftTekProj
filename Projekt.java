ec63cc6fc166f5b2d3f4c51ba9054f038d6b3bcd
Adam Fordsmand <indermandfrainderland@gmail.com>
2022-01-04 12:03:48 +0100

    Added beginnings of a Physics Engine

--- Projekt.java
+++ Projekt.java
@@ -1,10 +1,32 @@
+import java.util.Timer; 
+import java.util.TimerTask; 
+
+
 public class Projekt {
     public static void main(String[] args) {
         
-    }
 
-    public void shoot(long angle, long velocity) {
+        
+
+
+
+        // Start Physics engine
+        Timer timer = new Timer();
+        TimerTask task = new Physics();
+        timer.schedule(task, 0, 1);
 
     }
 }
 
+
+// Physics engine
+class Physics extends TimerTask {
+    public static int i = 0;
+    
+    public void run(){
+
+        // TODO: Calculate physics
+
+        System.out.print("Test");
+    }
+}
