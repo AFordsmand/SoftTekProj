398fda2a19998ab6ab5d6bd6e2509a6909b36efb
CBlixt <90332732+CBlixt@users.noreply.github.com>
2022-01-19 15:41:39 +0100

    Add files via upload
    Wind class

---
+++ Gorillas/src/main/java/dev/simpgorillas/model/entities/Wind.java
@@ -0,0 +1,17 @@
+package dev.simpgorillas.model.entities;
+
+public class Wind {
+    public int isWind;
+    public double windValue = 22;
+
+
+
+    public void setWind() {
+        if (isWind == 1){
+            windValue = (int)(Math.random() * (200) - 100) / 100.0;
+        }else{
+            windValue = 0;
+        }
+        System.out.println(windValue + "\n");
+    }
+}
